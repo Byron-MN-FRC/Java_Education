5a68132588cef684582659d42a5d98aa39f4d819
pbrink21 <pbrink21@live.com>
2017-11-17 01:06:26 -0600

    wowwweeeed

--- src/java_tutorial/HelloWorldDemo.java
+++ src/java_tutorial/HelloWorldDemo.java
@@ -5,7 +5,7 @@ package java_tutorial;
 public class HelloWorldDemo {  
 
 	public static void main(String[] args) {  
-        System.out.println("Poop Emoji is da best"); 
+        System.out.println("Poop Emoji is da iowhopian best"); 
 
     }  
 }  
