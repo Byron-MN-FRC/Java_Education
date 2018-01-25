af33e912723b06c47921678d325a96750792ecd4
morstad <morstad@us.ibm.com>
2018-01-24 18:31:30 -0600

    sample flow

---
+++ src/testflow/AutonSelector.java
@@ -0,0 +1,110 @@
+package testflow;
+
+
+public class AutonSelector {
+
+	private char location = ' ';
+	private char targetSide = ' ';
+	private char oppositeTargetSide = ' ';
+
+	public AutonSelector(char inputLocation, char inputTargetSide) {
+		location = inputLocation;
+		targetSide = inputTargetSide;
+		oppositeTargetSide = 'L'; // default
+		if (targetSide == 'L') { oppositeTargetSide = 'R'; }
+	}
+	
+	private void deliverCube() {
+		if (RobotMap.targetScale) {
+			System.out.println("Lift mechanism command");
+		} 
+		System.out.println("Deliver cube command");		
+	}
+	
+	private void driveForward() {
+		System.out.println("Drive to switch");	
+		if (RobotMap.targetScale) {
+			System.out.println("Driver further to scale");
+		} 
+	}
+
+	private void turn(char direction) {
+		System.out.println("Turn to " + direction);	
+	}
+
+	private void driveToWall() {
+		System.out.println("Drive toward wall");	
+		if (location == 'C') {
+			System.out.println("Drive further (started at center)");
+		}
+	}
+	
+	private void driveAroundSwitch(char direction) {
+		System.out.println("Drive Around Switch to " + direction);
+		if (RobotMap.targetScale) {
+			System.out.println("Driver further to scale");
+		} 
+	}
+
+	private void driveToTarget() {
+		
+		// Determine path to target based on starting position of robot
+		switch(location) {
+		case 'C':
+			turn(targetSide);
+			driveToWall();
+			turn(oppositeTargetSide);
+			driveForward();
+			deliverCube();
+			break;
+		case 'L':
+			turn('L');
+			driveToWall();
+			turn('R');
+			if (targetSide == 'L') {
+				driveForward();
+			} else {
+				driveAroundSwitch('R');
+			}
+			deliverCube();
+			break;
+		case 'R':
+			turn('R');
+			driveToWall();
+			turn('L');
+			if (targetSide == 'R') {
+				driveForward();
+			} else {
+				driveAroundSwitch('L');
+			}
+			deliverCube();
+			break;
+		default:
+			System.out.println("Drive forward");
+			break;
+		} 		
+	}
+
+
+	public static void main(String[] args) {
+
+		int delay = Integer.parseInt(args[0]);
+		String robotPos = args[1], gameData = args[2], targetScale = args[3];
+		
+		//RobotMap.delayInSeconds = (int) SmartDashboard.getNumber("Auton Delay", 0);
+		RobotMap.delayInSeconds = delay;
+		// String robotPos = SmartDashboard.getString("Robot Start Pos (L,R, or C)", "Non Received");
+		char location = Character.toUpperCase(robotPos.charAt(0));
+		// gameData = DriverStation.getInstance().getGameSpecificMessage();
+		char targetSide = gameData.charAt(0); // default to switch side
+		// String targetScale = SmartDashboard.getString("Target", "Non Received");  // ?? is there a boolean
+		if (targetScale.equalsIgnoreCase("Y")) { 
+			RobotMap.targetScale = true; 
+			targetSide = gameData.charAt(1);
+		} 
+
+		System.out.println("\n\n****  startingPosition=" + location + " targetSide=" + targetSide + " scale="+targetScale  );
+		AutonSelector x = new AutonSelector(location, targetSide);
+		x.driveToTarget();
+	}
+}
---
+++ src/testflow/RobotMap.java
@@ -0,0 +1,17 @@
+package testflow;
+
+public class RobotMap {
+	
+	public static int talonIDLeftMaster = 3;
+	public static int talonIDRightMaster = 1;
+	
+	public static int talonIDLeftFollower = 4;
+	public static int talonIDRightFollower = 2;
+	
+	public static boolean pMode = false;
+	
+	
+	public static boolean targetScale = false;
+	public static int delayInSeconds = 0;
+	
+}	
